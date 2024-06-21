91ef77a3c9647c2c51eb70e6d5c33c30317b96ee
Elias Vazquez <eliasvm@yahoo.com>
2024-06-21 12:56:21 -0600

    initial version

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+static void EntryPoint()
+{
+    string lastName = string.Empty;
+
+    if ( string.IsNullOrEmpty( lastName) )
+    {
+        throw new Exception("lastname variable cannot be null or empty.");
+    }
+
+    Console.WriteLine("End application.");
+}
+
+try{
+     EntryPoint();
+}
+catch(Exception ex) {
+    Console.WriteLine("\nCaught exception:");
+    Console.WriteLine("StackTrace data: ");
+    Console.WriteLine( ex.StackTrace);
+
+    Console.WriteLine("\nStackTrace by properties: ");
+    StackTrace stackTrace = new StackTrace(ex, true);
+
+    for (int inx = 0; inx < stackTrace.FrameCount; inx++) {
+        StackFrame frame = stackTrace.GetFrame(inx);
+
+        Console.WriteLine($"Method      : { frame.GetMethod().Name}");
+        Console.WriteLine($"File        : { frame.GetFileName() }");
+        Console.WriteLine($"Line number : { frame.GetFileLineNumber() }");
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#### Example about StackTrace when a exception occurs.
